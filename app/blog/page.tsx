33a05acb2f11443ad87696465b0700c6e30d09b8
Nicolas Gonzalez <nigd22@gmail.com>
2023-06-26 23:48:35 -0300

    feat: update blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from 'next';
-import Link from 'next/link';
-import { allBlogs } from 'contentlayer/generated';
 
 export const metadata: Metadata = {
   title: 'Blog',
-  description: 'Read my thoughts on software development, design, and more.',
+  description: 'Read my thoughts on front end development, music, and more.',
 };
 
 export default async function BlogPage() {
@@ -12,24 +10,6 @@ export default async function BlogPage() {
     <section>
       <h1 className='mb-5'>Blog</h1>
       <p>Under construction</p>
-      {/* {allBlogs
-        .sort((a, b) => {
-          if (new Date(a.publishedAt) > new Date(b.publishedAt)) {
-            return -1;
-          }
-          return 1;
-        })
-        .map((post) => (
-          <Link
-            key={post.slug}
-            className='flex flex-col space-y-1 mb-4'
-            href={`/blog/${post.slug}`}
-          >
-            <div className='w-full flex flex-col'>
-              <p>{post.title}</p>
-            </div>
-          </Link>
-        ))} */}
     </section>
   );
 }
