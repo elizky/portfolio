07367d98983724d09cde7ac2240aea472bf6834c
Nicolas Gonzalez <nigd22@gmail.com>
2024-10-09 09:24:57 -0300

    chore: :mag: change sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,55 +1,55 @@
-import { MetadataRoute } from "next";
+import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const currentDate = new Date();
 
   return [
     {
-      url: "https://www.izky.dev",
+      url: 'https://izky.dev',
       lastModified: currentDate,
-      changeFrequency: "yearly",
+      changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: "https://www.izky.dev/blog",
+      url: 'https://izky.dev/blog',
       lastModified: currentDate,
-      changeFrequency: "weekly",
+      changeFrequency: 'weekly',
       priority: 0.8,
     },
     {
-      url: "https://www.izky.dev/projects",
+      url: 'https://izky.dev/projects',
       lastModified: currentDate,
-      changeFrequency: "monthly",
+      changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: "https://www.izky.dev/blog/me",
+      url: 'https://izky.dev/blog/me',
       lastModified: currentDate,
-      changeFrequency: "yearly",
+      changeFrequency: 'yearly',
       priority: 0.7,
     },
     {
-      url: "https://www.izky.dev/projects/calculizky",
+      url: 'https://izky.dev/projects/calculizky',
       lastModified: currentDate,
-      changeFrequency: "monthly",
+      changeFrequency: 'monthly',
       priority: 0.7,
     },
     {
-      url: "https://www.izky.dev/projects/dividizky",
+      url: 'https://izky.dev/projects/dividizky',
       lastModified: currentDate,
-      changeFrequency: "monthly",
+      changeFrequency: 'monthly',
       priority: 0.7,
     },
     {
-      url: "https://www.izky.dev/projects/bitacorizky",
+      url: 'https://izky.dev/projects/bitacorizky',
       lastModified: currentDate,
-      changeFrequency: "monthly",
+      changeFrequency: 'monthly',
       priority: 0.7,
     },
     {
-      url: "https://www.izky.dev/projects/planizky",
+      url: 'https://izky.dev/projects/planizky',
       lastModified: currentDate,
-      changeFrequency: "monthly",
+      changeFrequency: 'monthly',
       priority: 0.7,
     },
   ];
